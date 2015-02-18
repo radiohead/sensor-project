b74857a2b9c916ac878597e0cce525a7f75d2566
Instant Contiki <user@instant-contiki.(none)>
2015-02-18 19:13:30 +0100

    at header file

--- sensor-mote/sensor-mote.h
+++ sensor-mote/sensor-mote.h
@@ -1,8 +1,15 @@
 #ifndef __UDP_CLIENT_H__
   #define __UDP_CLIENT_H__
 
-  #include "dev/temperature-sensor.h"
-  #include "dev/light-sensor.h"
+  //#include "dev/temperature-sensor.h"
+  //#include "dev/light-sensor.h"
+  
+  //Include these header file for light sensor driver
+  #include "dev/i2cmaster.h"
+  #include "dev/light-ziglet.h"
+
+  #include "dev/sht11.h"
+
   #include "dev/cc2420.h"
   #include "lib/random.h"
   #include "net/uip.h"
